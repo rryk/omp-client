04aa86bcb518852540c7d3e3aaa08801e3c55bfd
Rryk <rryk@ubuntu.(none)>
2013-03-12 22:09:35 +0100

    Refactoring. Implemented connection handling. Changed callback to be per call.

--- kiara.js
+++ kiara.js
@@ -7,6 +7,8 @@ KIARA.ErrorCode = {
   JQUERY_NOT_LOADED: "jQuery is not loaded",
   CANNOT_LOAD_IDL: "Cannot load IDL",
   UNKNOWN_TYPE: "Unknown type",
+  UNSUPPORTED_PROTOCOL: "Unsupported protocol",
+  WEBSOCKET_ERROR: "WebSocket error"
 }
 
 // Check for jQuery.
@@ -35,36 +37,36 @@ KIARA.DataType = function(kind, typeDefinition) {
     this.elementType = typeDefinition;
 }
 
+// Returns true if the type is a base type.
+KIARA.DataType.IsBaseType = function(typeName) {
+  return typeName == "boolean" || typeName == "i8" || typeName == "u8" || typeName == "i16" || typeName == "u16" || 
+         typeName == "i32" || typeName == "u32" || typeName == "i64" || typeName == "u64" || typeName == "float" ||
+         typeName == "double" || typeName == "string" || typeName == "void";
+}
+
+// Returns true if the type is an array.
+KIARA.DataType.IsArrayType = function(typeName) {
+  return typeName.endsWith("[]");
+}
+
+// Returns element type of an array.
+KIARA.DataType.GetArrayElementType = function(typeName) {
+  if (!KIARA.DataType.IsArrayType(typeName))
+    throw KIARA.ErrorCode.INVALID_ARGUMENT;
+  return typeName.substr(0, typeName.length - 2);
+}
+
 // Represents execution context. Operations in each context are completely indepedent from each other.
 KIARA.Context = function() {
   this._dataTypes = {};
 }
 
-// Returns true if the type is a base type.
-KIARA.Context.IsBaseType = function(typeName) {
-  return typeName == "boolean" || typeName == "i8" || typeName == "u8" || typeName == "i16" || typeName == "u16" || 
-         typeName == "i32" || typeName == "u32" || typeName == "i64" || typeName == "i64" || typeName == "float" ||
-         typeName == "double" || typeName == "string" || typename == "void";
-}
-
-// Returns true if the type is an array.
-KIARA.Context.IsArray = function(typeName) {
-  return typeName.endsWith("[]");
-}
-
-// Returns element type of an array.
-KIARA.Context.GetArrayElementType = function(typeName) {
-  if (!this.IsArrayType(typeName))
-    throw KIARA.ErrorCode.INVALID_ARGUMENT;
-  return typeName.substr(0, typeName.length - 2);
-}
-
 // Returns a named type.
 KIARA.Context.prototype.GetType = function(typeName) {
-  if (this.IsBaseType(typeName)) {
+  if (KIARA.DataType.IsBaseType(typeName)) {
     return new KIARA.DataType("base", typeName);
-  } else if (this.IsArrayType(typeName)) {
-    return new KIARA.DataType("array", this.GetType(this.GetArrayElementType(typeName)));
+  } else if (KIARA.DataType.IsArrayType(typeName)) {
+    return new KIARA.DataType("array", this.GetType(KIARA.DataType.GetArrayElementType(typeName)));
   } else if (this._dataTypes[typeName] != undefined) {
     return this._dataTypes[typeName];
   }
@@ -110,19 +112,165 @@ KIARA.Connection = function(context, url) {
   });
 }
 
-// Constructs a client function that will automatically serialize all arguments and send them over network. When 
-// response is received, it is deserialized and passed to a callback.
-KIARA.Connection.prototype.GenerateClientFunc = function(method, typeMapping, callback) {
+// Constructs a client function that will automatically serialize all arguments and send them over network. The function
+// will return an empty object, which can be used to set up a callback by assigining it to the callback field:
+//   var login = conn.GenerateClientFunc(...);
+//   login().callback = function(response) { ... };
+// Callback will be called with return value of the function.
+KIARA.Connection.prototype.GenerateClientFunc = function(method, typeMapping) {
   var parsedMapping = this._parseTypeMapping(method, typeMapping);
-  var connection = this._getConnection(this._services[method]);
+  var that = this;
   return function() {
-    var rawData = parsedMapping.serializer(arguments);
-    connection.send(rawData);
-    connection.ondata = function(data) {
-      callback(parsedMapping.deserializer(data));
+    var serializedData = parsedMapping.serializer(arguments);
+    var connection = that._getConnection(method);
+    var callbackContainer = {};
+    connection.send(serializedData);
+    connection.ondata = function(serializedData) {
+      if (callbackContainer.callback != undefined)
+        callbackContainer.callback(parsedMapping.deserializer(serializedData));
       delete connection.ondata;
     }
+    return callbackContainer;
   }
+}
+
+KIARA.Connection._getConnection = function(qualifiedMethodName) {
+  var localMethodName = qualifiedMethodName.substr(this._namespace.length);
+  var serviceName = localMethodName.substr(0, localMethodName.indexOf("."));
+  var methodName = localMethodName.substr(localMethodName.indexOf("."));
+  var service = this._services[serviceName];
+  var method = service.methods[methodName];
+  
+  if (method.connection == undefined) {
+     if (service.protocol != "WebSocket")
+       throw KIARA.ErrorCode.UNSUPPORTED_PROTOCOL;
+     method.connection = new WebSocket(service.baseURL + methodName);
+     method.connection.onerror = function() { throw KIARA.ErrorCode.WEBSOCKET_ERROR; }
+     method.connection.onclose = function() { delete method.connection; }  // will trigger reconnect at next call
+  }
+  
+  return method.connection;
+}
+
+KIARA.Connection._getValue = function(argument, path) {
+  var value = argument;
+  for (var index in path)
+    value = value[path[index]];
+  return value;
+}
+
+KIARA.Connection._setValue = function(result, path, value) {
+  var cell = result;
+
+  // Construct necessary arrays/objects first.
+  for (var index = 0; index < path.length - 1; index++) {
+    var indexOrField = path[index];
+    if (cell[indexOrField] == undefined) {
+      if (typeof path[index+1] == "number")
+        cell[indexOrField] = [];
+      else if (typeof path[index+1] == "string")
+        cell[indexOrField] = {};
+      else
+        throw KIARA.ErrorCode.INVALID_ARGUMENT;
+    }
+    cell = cell[indexOrField];
+  }
+
+  cell[path[path.length-1]] = value;
+}
+
+KIARA.Connection._getRawData = function(arguments, serializeInfo) {
+  var rawData = [];
+  for (var entryIndex in serializeInfo) {
+     var entry = serializeInfo[entryIndex];
+     switch (entry.type) {
+       case "zc-string":
+   var str = KIARA.Connection._getValue(arguments, entry.path);
+         for (var i = 0; i < str.length; i++)
+           rawData.push(["u16", str.charCodeAt(i)]);
+         rawData.push(["u16", 0]);
+         break;
+       case "array":
+         var array = KIARA.Connection._getValue(entry.path);
+         rawData.push(["u32", array.length]);
+         for (var i = 0; i < array.length; i++) {
+           var nestedData = KIARA.Connection._getRawData(array[i], entry.nested);
+           rawData = rawData.concat(nestedData);
+         }
+         break;
+       case "u8":
+       case "i8":
+       case "u16":
+       case "i16":
+       case "u32":
+       case "i32":
+         var value = KIARA.Connection._getValue(arguments, entry.path);
+         rawData.push([entry.type, value]);
+         break;
+       case "float":
+         var value = KIARA.Connection._getValue(arguments, entry.path);
+         rawData.push(["f32", value]);
+         break;
+       case "double":
+         var value = KIARA.Connection._getValue(arguments, entry.path);
+         rawData.push(["f64", value]);
+         break;
+       case "enum":
+         var value = KIARA.Connection._getValue(arguments, entry.path);
+         if (value in entry.enumDict)
+           rawData.push(["u32", entry.enumDict[value]]);
+         else
+           rawData.push(["u32", entry.enumDefault]);
+         break;
+       default:
+         throw KIARA.ErrorCode.INVALID_ARGUMENT;
+     }
+  }
+  return rawData;
+}
+
+KIARA.Connection._universalSerializer = function(arguments, info) {
+  var rawData = KIARA.Connection._getRawData(arguments, info);
+  
+  var typeMetaData = {
+    "u8": [Uint8Array, 1],
+    "i8": [Int8Array, 1],
+    "u16": [Uint16Array, 2],
+    "i16": [Int16Array, 2],
+    "u32": [Uint32Array, 4],
+    "i32": [Int32Array, 4],
+    "f32": [Float32Array, 4],
+    "f64": [Float64Array, 8],
+  }
+
+  var numBytes = 0;
+  for (var i = 0; i < rawData.length; i++) {
+    var typeName = rawData[i][0];
+    var typeSize = typeMetaData[typeName][1];
+    
+    numBytes += typeSize;
+  }
+    
+  var buffer = new ArrayBuffer(numBytes);
+  var offset = 0;
+  for (var i = 0; i < rawData.length; i++) {
+    var typeName = rawData[i][0];
+    var value = rawData[i][1];
+    
+    var typeSize = typeMetaData[typeName][1];
+    var typedArrayType = typeMetaData[typeName][0];
+    var typedArray = new typedArrayType(buffer, offset, 1);
+    
+    typedArray[0] = value;
+    
+    offset += typeSize;
+  }
+  
+  return buffer;
+}
+
+KIARA.Connection._universalDeserializer = function(result) {
+  // TODO(rryk): Implement deserializer.
 }
 
 // Private. Parses type mapping string and return an object containing serializer for arguments and deserializer for
@@ -231,142 +379,23 @@ KIARA.Connection.prototype._parseTypeMapping = function(method, typeMapping) {
   var resultDeserializeInfo;
 
   // Currently it's just a hack that has pre-coded functions for each method.
+  // TODO(rryk): Hard-code type mapping info for login_to_simulator and set_login_level
   if (method == "opensim.login.login_to_simulator") {
-
-  }
-
-  function getValue(argument, path) {
-    var value = argument;
-    for (var index in path)
-      value = value[path[index]];
-    return value;
-  }
-
-  function setValue(result, path, value) {
-    var cell = result;
-
-    // Construct necessary arrays/objects first.
-    for (var index = 0; index < path.length - 1; index++) {
-      var indexOrField = path[index];
-      if (cell[indexOrField] == undefined) {
-        if (typeof path[index+1] == "number")
-          cell[indexOrField] = [];
-        else if (typeof path[index+1] == "string")
-          cell[indexOrField] = {};
-        else
-          throw KIARA.ErrorCode.INVALID_ARGUMENT;
-      }
-      cell = cell[indexOrField];
-    }
-
-    cell[path[path.length-1]] = value;
-  }
-
-  function getRawData(arguments, serializeInfo) {
-    var rawData = [];
-    for (var entryIndex in serializeInfo) {
-       var entry = serializeInfo[entryIndex];
-       switch (entry.type) {
-         case "zc-string":
-	   var str = getValue(arguments, entry.path);
-           for (var i = 0; i < str.length; i++)
-             rawData.push(["u16", str.charCodeAt(i)]);
-           rawData.push(["u16", 0]);
-           break;
-         case "array":
-           var array = getValue(entry.path);
-           rawData.push(["u32", array.length]);
-           for (var i = 0; i < array.length; i++) {
-             var nestedData = getRawData(array[i], entry.nested);
-             rawData = rawData.concat(nestedData);
-           }
-           break;
-         case "u8":
-         case "i8":
-         case "u16":
-         case "i16":
-         case "u32":
-         case "i32":
-           var value = getValue(arguments, entry.path);
-           rawData.push([entry.type, value]);
-           break;
-         case "float":
-           var value = getValue(arguments, entry.path);
-           rawData.push(["f32", value]);
-           break;
-         case "double":
-           var value = getValue(arguments, entry.path);
-           rawData.push(["f64", value]);
-           break;
-         case "enum":
-           var value = getValue(arguments, entry.path);
-           if (value in entry.enumDict)
-             rawData.push(["u32", entry.enumDict[value]]);
-           else
-             rawData.push(["u32", entry.enumDefault]);
-           break;
-         default:
-           throw KIARA.ErrorCode.INVALID_ARGUMENT;
-       }
-    }
-    return rawData;
-  }
-
-  function universalSerializer(arguments) {
-    var rawData = getRawData(arguments, argsSerializeInfo);
     
-    var typeMetaData = {
-      "u8": [Uint8Array, 1],
-      "i8": [Int8Array, 1],
-      "u16": [Uint16Array, 2],
-      "i16": [Int16Array, 2],
-      "u32": [Uint32Array, 4],
-      "i32": [Int32Array, 4],
-      "f32": [Float32Array, 4],
-      "f64": [Float64Array, 8],
-    }
-
-    var numBytes = 0;
-    for (var i = 0; i < rawData.length; i++) {
-      var typeName = rawData[i][0];
-      var typeSize = typeMetaData[typeName][1];
-      
-      numBytes += typeSize;
-    }
-      
-    var buffer = new ArrayBuffer(numBytes);
-    var offset = 0;
-    for (var i = 0; i < rawData.length; i++) {
-      var typeName = rawData[i][0];
-      var value = rawData[i][1];
-      
-      var typeSize = typeMetaData[typeName][1];
-      var typedArrayType = typeMetaData[typeName][0];
-      var typedArray = new typedArrayType(buffer, offset, 1);
-      
-      typedArray[0] = value;
-      
-      offset += typeSize;
-    }
-    
-    return buffer;
-  }
-
-  function universalDeserializer(result) {
-    // TODO(rryk): Implement.
+  } else if (method == "opensim.login.set_login_level") {
+  
   }
 
   return {
-    serializer:universalSerializer,
-    deserializer: function(result) {
-    },
+    serializer: function() { KIARA.Connection._universalSerializer(arguments, argsSerializeInfo); },
+    deserializer: function() { KIARA.Connection._universalDeserializer(arguments, argsSerializeInfo); },
   };
 }
 
 // Private. Parses downloaded IDL and stores internal representation of it.
 KIARA.Connection.prototype._parseIDL = function(url, idl) {
   // Currently it's just a hack that uses dictionary for a set of known IDLs.
-  if (url == "http://yellow.cs.uni-saarland.de/home/kiara/login.idl") {
+  if (url == "http://localhost/home/kiara/login.idl") {
     this._namespace = "opensim";
     this._addStructType("FullName", {
       first: "string", 
@@ -403,12 +432,12 @@ KIARA.Connection.prototype._parseIDL = function(url, idl) {
       region_x: "i32",
       seed_capability: "string",
       agent_access: "AccessType",
-      session_id: "session_id",
+      session_id: "string",
     });
-    this._addService("login", {
+    this._addService("login", "WebSocket", "ws://yellow.cs.uni-saarland.de:8002/", {
       "login_to_simulator": [ "LoginResponse", [ "LoginRequest" ] ],
       "set_login_level": [ "boolean", [ "FullName", "string" ] ],
-    }, "WebSocket", "http://yellow.cs.uni-saarland.de:8002/");
+    });
   }
 }
 
@@ -417,19 +446,19 @@ KIARA.Connection.prototype._parseIDL = function(url, idl) {
 KIARA.Connection.prototype._prependNamespaceIfNeeded = function(type) {
   // Unwrap (multidimensional) array and get the element type.
   var elementType = type;
-  while (this._context.IsArrayType(elementType)) {
-    elementType = this._context.GetArrayElementType(elementType);
+  while (KIARA.DataType.IsArrayType(elementType)) {
+    elementType = KIARA.DataType.GetArrayElementType(elementType);
   }
 
   // Base types are defined in the global namespace. Do not prepend our namespace.
-  if (this._context.IsBaseType(elementType))
-    return;
+  if (KIARA.DataType.IsBaseType(elementType))
+    return type;
 
   // Check if the type is defined in our namespace and prepend it if so.
   var typeInNamespace = this._namespace + "." + type;
   try {
    this._context.GetType(typeInNamespace);
-   type = typeInNamespace;
+   return typeInNamespace;
   } catch (error) {
     if (error != KIARA.ErrorCode.UNKNOWN_TYPE)
       throw error;  // any other error should be propagated further
@@ -442,9 +471,9 @@ KIARA.Connection.prototype._prependNamespaceIfNeeded = function(type) {
 // Private. Adds a struct type. Prepends namespaces where necessary.
 KIARA.Connection.prototype._addStructType = function(name, def) {
   for (var fieldName in def)
-    def[fieldName] = prependNamespaceIfNeeded(def[fieldName]);
+    def[fieldName] = this._prependNamespaceIfNeeded(def[fieldName]);
   
-  this._context.AddStructType(this._namespace + "." + name, def); 
+  this._context.AddStructType(this._namespace + "." + name, def);
 }
 
 // Private. Adds an enumeration type. Prepends namespaces where necessary.
@@ -453,19 +482,20 @@ KIARA.Connection.prototype._addEnumType = function(name, def) {
 }
 
 // Private. Adds a service. Prepends namespaces where necessary.
-KIARA.Connection.prototype._addService = function(name, methods, protocol, url) {
+KIARA.Connection.prototype._addService = function(name, protocol, baseURL, methods) {
   var newMethods = {};
   for (var methodName in methods) {
     var methodDef = methods[methodName];
+    newMethods[methodName] = {};
     newMethods[methodName].response = this._prependNamespaceIfNeeded(methodDef[0]);
     newMethods[methodName].request = [];
     for (var argIndex in methodDef[1])
       newMethods[methodName].request[argIndex] = this._prependNamespaceIfNeeded(methodDef[1][argIndex]);
   }
 
-  this._services[this._namespace + "." + name] = {
+  this._services[name] = {
     methods: newMethods,
     protocol: protocol,
-    url: url
+    baseURL: baseURL
   };
 }
